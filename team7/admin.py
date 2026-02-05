778ef1a0df9f1d2a1590c9b98e6ed23b85429524
mahanzavari <mahanzavari@gmail.com>
2026-02-05 21:07:59 +0330

    updated admin

--- team7/admin.py
+++ team7/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question, Evaluation, DetailedScore
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_id', 'task_type', 'mode', 'difficulty')
+
+class DetailedScoreInline(admin.TabularInline):
+    model = DetailedScore
+    extra = 0
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('evaluation_id', 'user_id', 'overall_score', 'created_at')
+    inlines = [DetailedScoreInline]
