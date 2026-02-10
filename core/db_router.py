abb2f597b4b2a91a8ff288258a3f3fcda0f2456c
Amirhossein <amirmemool12@gmail.com>
2026-02-10 18:23:06 +0330

    fix db

--- core/db_router.py
+++ core/db_router.py
@@ -11,7 +11,16 @@ class TeamPerAppRouter:
             return model._meta.app_label
         return None
 
+    def allow_relation(self, obj1, obj2, **hints):
+        db1 = self.db_for_read(obj1.__class__) if hasattr(obj1, '_meta') else None
+        db2 = self.db_for_read(obj2.__class__) if hasattr(obj2, '_meta') else None
+
+        if db1 is None or db2 is None:
+            return None
+
+        return db1 == db2
+
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in settings.TEAM_APPS:
             return db == app_label
-        return db == "default"
+        return None
