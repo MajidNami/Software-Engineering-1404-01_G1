8a4183de2413daaaf35a77747d2349a8c79dcf2c
Parsa Eslami <p.eslami@yektanet.ir>
2026-02-11 20:43:52 +0330

    Add admin for team13 models.

---
+++ team13/models/prompt_admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from team13.models import Prompt
+
+
+@admin.register(Prompt)
+class PromptAdmin(admin.ModelAdmin):
+    list_display = ['name', 'prompt_text', 'created_at', 'modified_at']
+    search_fields = ['name']
+    readonly_fields = ['created_at', 'modified_at']
