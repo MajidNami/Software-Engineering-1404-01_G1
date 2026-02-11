0388b9a84a7a3a5a8a8886b53b04449f8c3e09bd
xBouncyBearx <hosseinismm@yahoo.com>
2026-02-11 22:53:31 +0330

    create view lessons_list_view for the list of lessons

--- team2/views.py
+++ team2/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
 from core.auth import api_login_required
+from team2.models import Lesson
 
 TEAM_NAME = "team2"
 
@@ -10,3 +13,20 @@ def ping(request):
 
 def base(request):
     return render(request, f"{TEAM_NAME}/index.html")
+
+
+@require_http_methods(["GET"])
+def lessons_list_view(request):
+
+    lessons = Lesson.objects.filter(
+        # TODO : add filter user
+        is_deleted=False,
+        status='published'
+    ).prefetch_related('videos')
+
+    context = {
+        'lessons': lessons,
+        'total_lessons': lessons.count(),
+    }
+    # TODO : create team2_Lessons_list.html
+    return render(request, 'team2_Lessons_list.html', context)
