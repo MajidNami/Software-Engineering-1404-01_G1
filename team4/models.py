f1696c41e5149beb4715e14e3de78614b6e32e2c
AmirrezaSabbagh <sbaghamyrrda96@gmail.com>
2026-02-12 23:20:30 +0330

    message text

--- team4/models.py
+++ team4/models.py
@@ -82,7 +82,6 @@ class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_pic_url = models.URLField(max_length=500, blank=True, null=True)
     is_online = models.BooleanField(default=False)
-
     last_seen = models.DateTimeField(auto_now=True)     
 
     def __str__(self):
