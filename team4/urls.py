60a67b76f02ece7d2eae4b9e4a05779727fa39ad
kasrarezaee <kasra.re138406@gmail.com>
2026-02-12 21:55:07 +0330

    adding urls

--- team4/urls.py
+++ team4/urls.py
@@ -1,7 +1,55 @@
+# from django.urls import path
+
+# urlpatterns = [
+# ]
+
+
+from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path("", views.base),
     path("ping/", views.ping),
+    path('admin/', admin.site.urls),
+    
+    # Registration
+    path('api/register/', RegisterView.as_view(), name='register'),
+    
+    # Login: Send username & password, get Access and Refresh tokens
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    # Refresh: Send Refresh token, get a new Access token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/test-token/', test_token_view, name='test_token'),
+
+    # path('api/search-users/', UserSearchView.as_view(), name='search_users'),
+    # path('api/my-chats/', MyChatRoomsView.as_view(), name='my_chats'),
+    # path('api/chat-history/<int:chat_id>/', ChatHistoryView.as_view(), name='chat_history'),
+    path('api/users/search/', UserSearchAPIView.as_view(), name='user_search'),
+    path('api/chat/start/', StartPrivateChatAPIView.as_view(), name='start_chat'),
+    path('api/chat/inbox/', MyChatRoomsView.as_view(), name='inbox'),
+    path('api/chat/history/<int:chat_id>/', ChatHistoryView.as_view(), name='history'),
+    path('api/chat/groups/create/', CreateGroupChatView.as_view(), name='create_group'),
+    path('api/chat/groups/<int:chat_id>/add-member/', AddMemberToGroupView.as_view(), name='add_member'),
+    path('api/chat/upload/', AttachmentUploadView.as_view()),
+    path('api/safety/block/', BlockUserView.as_view(), name='block_user'),
+    path('api/safety/report/', ReportUserView.as_view(), name='report_user'),
+
 ]
+
+
+
+# Add this to the very bottom
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
